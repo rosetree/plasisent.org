5b5506019c49a87f052fdd4cc0f430e51174b63c
Micha Rosenbaum <micha@rosetree.de>
2015-11-03 18:15:07 +0100

    Fix bug with `attribute_to_time` and a DateTime
    This is small bug I want to merge into the nanoc source code. I am
    currently waiting for an information, in which branch such a fix is
    supposed to be committed
    
    GitHub-Issue: https://github.com/nanoc/nanoc/issues/716

--- lib/default.rb
+++ lib/default.rb
@@ -138,6 +138,16 @@ end
 
 
 module Nanoc
+  module Helpers::Blogging
+    # TODO: Contribute to https://github.com/nanoc/nanoc
+    def attribute_to_time(time)
+      time = time.to_time if time.is_a?(DateTime)
+      time = Time.local(time.year, time.month, time.day) if time.is_a?(Date)
+      time = Time.parse(time) if time.is_a?(String)
+      time
+    end
+  end
+
   class Item
     def github_url base = ''
       return "" unless self[:content_filename]
