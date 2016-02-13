rosetree/plasisent.org

commit bf125f45553c3d0da9d1a2fa4713338d04eb50e8

Add yearly and weekly archive generation and layout
Add preprocessor Rules that will generate the archives.

Add a special layout for archives and render correct layout for
archives.

Add functions that generate the archives. This code is copied and
adapted from the CCCS [0] website. The original author is Skyr [1] and
their code is released under the CC BY-SA 3.0 Licence [2].

[0]:
https://github.com/cccs/cccs-website/blob/master/lib/yearlyarchive.rb

[1]:
https://github.com/Skyr

[2]:
http://creativecommons.org/licenses/by-sa/3.0/

lib/weekly_archive.rb
@@ -1,0 +1,65 @@
+# Original Source:
+#   https://github.com/cccs/cccs-website/blob/master/lib/yearlyarchive.rb
+#
+# Original Author:
+#   Stefan Schlott
+#   https://github.com/Skyr
+#
+# Original Licence:
+#   CC BY-SA 3.0
+#   http://creativecommons.org/licenses/by-sa/3.0/
+#
+def generate_weekly_archive(articles, date_attribute, title, week_url_prefix = "kw-")
+  today = Time.now
+  currentyear = today.year
+  yearmap = articles.group_by { |item| item[date_attribute].year }
+
+  if !yearmap.has_key?(currentyear)
+    yearmap[currentyear] = []
+  end
+
+  yearlist = yearmap.keys.sort
+
+  yearlist.each_index do |i|
+    year = yearlist[i]
+
+    weekmap = yearmap[year].group_by { |item| item[date_attribute].strftime("%V") }
+
+    weeklist = weekmap.keys.sort
+
+    weeklist.each_index do |wi|
+      week = weeklist[wi]
+
+      weekmap[week].sort! { |a,b| b[date_attribute].to_datetime <=> a[date_attribute].to_datetime }
+
+      articles_tags = []
+      weekmap[week].each do |a|
+        articles_tags << a[:tags] if a[:tags]
+      end
+      articles_tags = articles_tags.flatten.compact.uniq
+
+      linkprev = nil
+      linknext = nil
+
+      if (wi > 0)
+        linkprev = { :title => "#{title} #{weeklist[wi - 1]}", :link => "/#{year}/#{week_url_prefix}#{weeklist[wi - 1]}/" }
+      end
+
+      if (wi < weeklist.size - 1)
+        linknext = { :title => "#{title} #{weeklist[wi + 1]}", :link => "/#{year}/#{week_url_prefix}#{weeklist[wi + 1]}/" }
+      end
+
+      @items << Nanoc::Item.new(
+        "",
+        {
+          :title => "#{title} #{week}",
+          :kind => "archive",
+          :posts => weekmap[week],
+          :linkprev => linkprev,
+          :linknext => linknext,
+          :referenced_tags => articles_tags
+        },
+        "/#{year}/#{week_url_prefix}#{week}/")
+    end
+  end
+end

Rules
@@ -16,6 +16,9 @@
 preprocess do
   move_post_identifiers
   create_tag_pages
+  generate_yearly_archive(@items.find_all { |i| i[:kind] == 'article' }, :created_at, 'Jahr')
+  # TODO: Add new tags to the weekly archive. `|| i[:kind] == 'tag'`
+  generate_weekly_archive(@items.find_all { |i| i[:kind] == 'article' }, :created_at, 'KW')
 end
 
 compile '*' do

layouts/archive.haml
@@ -1,0 +1,24 @@
+%h1= item[:title]
+
+- if item[:linkprev] || item[:linknext]
+  %p
+    - if item[:linkprev]
+      = link_to item[:linkprev][:title], item[:linkprev][:link]
+      ←
+    - if item[:linknext]
+      →
+      = link_to item[:linknext][:title], item[:linknext][:link]
+
+- if item[:posts]
+  %ol
+    - item[:posts].each do |i|
+      %li<
+        = i[:created_at].strftime("%d %B %Y")
+        %br/
+        = link_to i[:title], i.path
+
+-#%h2 Neue Themen -# TODO
+ 
+- if item[:referenced_tags]
+  %h2 Angesprochene Themen
+  %p= link_tags item, :referenced_tags

layouts/default.haml
@@ -27,6 +27,9 @@
     - when "tag"
       = render "tag" do
         = yield
+    - when "archive"
+      = render "archive" do
+        yield
     - when "article"
       -# TODO: I am not sure, whether or not I should add the headline to an
           article in the layout or in the text …

lib/yearly_archive.rb
@@ -1,0 +1,49 @@
+# Original Source:
+#   https://github.com/cccs/cccs-website/blob/master/lib/yearlyarchive.rb
+#
+# Original Author:
+#   Stefan Schlott
+#   https://github.com/Skyr
+#
+# Original Licence:
+#   CC BY-SA 3.0
+#   http://creativecommons.org/licenses/by-sa/3.0/
+#
+def generate_yearly_archive(articles, date_attribute, title)
+  today = Time.now
+  currentyear = today.year
+  yearmap = articles.group_by { |item| item[date_attribute].year }
+
+  if !yearmap.has_key?(currentyear)
+    yearmap[currentyear] = []
+  end
+
+  yearlist = yearmap.keys.sort
+
+  yearlist.each_index do |i|
+    year = yearlist[i]
+    yearmap[year].sort! { |a,b| b[date_attribute].to_datetime <=> a[date_attribute].to_datetime }
+
+    linkprev = nil
+    linknext = nil
+
+    if (i > 0)
+      linkprev = { :title => "#{title} #{yearlist[i - 1]}", :link => "/#{yearlist[i - 1]}/" }
+    end
+
+    if (i < yearlist.size - 1)
+      linknext = { :title => "#{title} #{yearlist[i + 1]}", :link => "/#{yearlist[i + 1]}/" }
+    end
+
+    @items << Nanoc::Item.new(
+      "",
+      {
+        :title => "#{title} #{year}",
+        :kind => "archive",
+        :posts => yearmap[year],
+        :linkprev => linkprev,
+        :linknext => linknext,
+      },
+      "/#{year}/")
+  end
+end

commit 0bd1bf2dddc685d2833f4747e93f9a6a5a3c4731

Use week year in the weekly archives
It is possible, that a post is written in a day of year one, but the
week is already year two – or the other way round. It happened in
Plasisent at least once, in 2015-W01 [0].

Quoting the offical Ruby documentation from Time#strftime [1]:

> ISO 8601 week-based year and week number:
> The first week of YYYY starts with a Monday and includes YYYY-01-04.
> The days in the year before the first week are in the last week of
> the previous year.
>   %G - The week-based year

[0] http://plasisent.org/0ecv002p
[1] http://ruby-doc.org/core-2.3.0/Time.html#method-i-strftime

See-Also: http://www.cl.cam.ac.uk/~mgk25/iso-time.html
Fixes: bf125f45553c3 Add yearly and weekly archive generation and layout

lib/default.rb
@@ -225,12 +225,13 @@ module Nanoc
       date = self[:created_at]
 
       year  = date.strftime "%Y"
+      wyear = date.strftime "%G"
       month = german_month_name date
       day   = date.strftime "%-d"
 
       time_elem = "<time datetime='#{date.iso8601}'>#{day}. #{month}</time>"
       year_link = link_to year, "/#{year}/"
-      week_link = link_to "Woche #{date.strftime("%-V")}", "/#{year}/kw-#{date.strftime("%V")}/"
+      week_link = link_to "Woche #{date.strftime("%-V")}", "/#{wyear}/kw-#{date.strftime("%V")}/"
 
       "#{time_elem} (#{year_link}, #{week_link})"
     end

lib/weekly_archive.rb
@@ -11,7 +11,7 @@
 #
 def generate_weekly_archive(articles, date_attribute, title, week_url_prefix = "kw-")
   today = Time.now
-  currentyear = today.year
+  currentyear = today.strftime('%G').to_i
 
   # FIXME: It shouldn’t be necessary to check type of the date_attribute here.
   articles.each do |item|
@@ -20,7 +20,7 @@ def generate_weekly_archive(articles, date_attribute, title, week_url_prefix = "
     end
   end
 
-  yearmap = articles.group_by { |item| item[date_attribute].year }
+  yearmap = articles.group_by { |i| i[date_attribute].strftime('%G').to_i }
 
   if !yearmap.has_key?(currentyear)
     yearmap[currentyear] = []