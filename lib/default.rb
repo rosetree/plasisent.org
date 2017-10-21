8763afc3a15f1421286ce831e4c9b496c71b2321
Micha Rosenbaum <micha@rosetree.de>
2017-10-21 11:11:35 +0200

    Add global methods page_shortlink and base_36_code

--- lib/default.rb
+++ lib/default.rb
@@ -79,47 +79,47 @@ def reply_to_mailto_link
   link_to e, l, {rel: 'reply-to'}
 end
 
+def base_36_code
+  date = attribute_to_time @item[:created_at]
+
+  if @item[:base_36_code]
+    return @item[:base_36_code]
+  end
+
+  unless date
+    return nil # TODO: Throw an error / warning?
+  end
+
+  # Special handling for year and time
+  #
+  # To save a character, I’m going to assume, we have write all articles in
+  # the 2000 millenium.
+  year = (date.year - 2000)
+  # This is smaller, than using base 36 versions of each, and I don’t
+  # think, my blog is going to need a hackable time url.
+  time = "#{date.hour}#{date.min}#{date.sec}".to_i
+
+  # Create short identifier with base 36
+  code =              year.to_s(36).rjust(2, "0")
+  code = code + date.month.to_s(36)
+  code = code +   date.day.to_s(36)
+  code = code +       time.to_s(36).rjust(4, "0")
+
+  return code
+end
+
+def page_shortlink
+  return nil unless base_36_code
+  # Check if the shortlink is shorter than the long link.
+  return nil if @item.path.length <= base_36_code.length
+  # We use File.join to make sure there is a slash between domain and page
+  # TODO: Make the domain configurable
+  File.join('http://plasisent.org/', base_36_code)
+end
+
 
 module Nanoc
   class CompilationItemView
-    def base_36_code
-      date = attribute_to_time self[:created_at]
-
-      if self[:base_36_code]
-        return self[:base_36_code]
-      end
-
-      unless date
-        return nil # TODO: Throw an error / warning?
-      end
-
-      # Special handling for year and time
-      #
-      # To save a character, I’m going to assume, we have write all articles in
-      # the 2000 millenium.
-      year = (date.year - 2000)
-      # This is smaller, than using base 36 versions of each, and I don’t
-      # think, my blog is going to need a hackable time url.
-      time = "#{date.hour}#{date.min}#{date.sec}".to_i
-
-      # Create short identifier with base 36
-      code =              year.to_s(36).rjust(2, "0")
-      code = code + date.month.to_s(36)
-      code = code +   date.day.to_s(36)
-      code = code +       time.to_s(36).rjust(4, "0")
-
-      return code
-    end
-
-    def shortlink
-      return nil unless self.base_36_code
-      # Check if the shortlink is shorter than the long link.
-      return nil if self.path.length <= self.base_36_code.length
-      # We use File.join to make sure there is a slash between domain and page
-      # TODO: Make the domain configurable
-      File.join('http://plasisent.org/', self.base_36_code)
-    end
-
     def linked_date_information
       return "" unless self[:created_at]
 
